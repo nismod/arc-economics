95e7f05b4b2e69bed16eba01d3e180c6d16d8c20
Tom Russell <tomalrussell@gmail.com>
2019-08-30 13:15:25 +0100

    Handle new scenario variants

--- post-population.py
+++ post-population.py
@@ -22,7 +22,7 @@ def main(base_path):
             'arc_population__', '').replace('.csv', '')
 
         # HACK hard-code match for economics scenarios against 23k dwellings scenarios
-        if key == "3-new-cities23":
+        if "new-cities" in key:
             econ_key = "1-new-cities"
         elif key == "4-expansion23":
             econ_key = "2-expansion"
